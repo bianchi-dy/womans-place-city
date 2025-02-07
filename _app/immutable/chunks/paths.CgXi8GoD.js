9cc1ee90b475616826d27c4b251b0033ae1d15a3
Bianchi Dy <bianchi@kontinentalist.com>
2025-02-06 23:45:27 -0500

    Updates

---
+++ _app/immutable/chunks/paths.CgXi8GoD.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_g0pbst)==null?void 0:s.base)??"/womans-place-city";var t;const e=((t=globalThis.__sveltekit_g0pbst)==null?void 0:t.assets)??a;export{e as a,a as b};
